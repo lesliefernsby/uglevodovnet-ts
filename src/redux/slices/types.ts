fc435aecec9918f155a97209f6356fd8c0218e4b
Leslie <leslie.fernsby@gmail.com>
2022-07-10 13:50:31 -0400

    small fix2

--- src/redux/slices/types.ts
+++ src/redux/slices/types.ts
@@ -1,12 +1,18 @@
-
 export type TUser = {
-  login: string,
-  firstName?: string,
-  middleName?: string,
-  lastName?: string,
-  roles: string[],
+  login: string;
+  firstName?: string;
+  middleName?: string;
+  lastName?: string;
+  roles: string[];
   tokens: {
-    accessToken: string,
-    refreshToken: string
-  }
-}
+    accessToken: string;
+    refreshToken: string;
+  };
+};
+
+export type TUserError = {
+  detail: string;
+  status: number;
+  title: string;
+  type: string;
+};
